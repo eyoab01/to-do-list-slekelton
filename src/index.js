a4862dfd128ec5abf81fc05fb30bea0a9d053011
tesfaye <contactesfaye>
2023-03-04 15:53:38 +0300

    to do list skeleton

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './style.css';
+// arrow todolistect
+const todolist = [
+  {
+    discription: 'js',
+    completed: '',
+    index: 1,
+  },
+  {
+    discription: 'java',
+    completed: '',
+    index: 2,
+  },
+  {
+    discription: 'html',
+    completed: '',
+    index: 3,
+  },
+];
+// display
+const list = document.querySelector('.todolist');
+todolist.forEach((o) => {
+  list.innerHTML += `
+  <div class = "tree"> <div class = "list">
+    <input type="checkbox" id="do1" name="do1" value="list">
+    <h2>${o.discription}</h2>
+    </div>
+    <i class="fa fa-trash fa-2x delete"></i>
+    <i class="fa delete1">&#xf142;</i></div>
+   
+  `;
+});
